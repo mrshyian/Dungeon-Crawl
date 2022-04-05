551132f0051c36b948aa786eed376aecdee8600e
justyna-eevee <justyna.zaczyk@onet.pl>
2022-04-05 15:45:34 +0200

    Create new models and interfaces

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -9,23 +9,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameStateDaoJdbc implements GameStateDao {
+
+    private DataSource dataSource;
+    private PlayerDaoJdbc playerDaoJdbc;
+
+    public GameStateDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.playerDaoJdbc = new PlayerDaoJdbc(dataSource);
+    }
+
     @Override
     public void add(GameState state) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO game_state (saved_at, current_map, player_name) VALUES (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setDate(1, state.getSavedAt());
+            statement.setString(2, state.getCurrentMap());
+            statement.setString(3, state.getPlayer().getPlayerName());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            state.setId(resultSet.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(GameState state) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "UPDATE game_state SET saved_at = ?, current_map = ? WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setDate(1, state.getSavedAt());
+            statement.setString(2, state.getCurrentMap());
+            statement.setInt(3, state.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public GameState get(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT saved_at, current_map, player_name FROM game_state WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet game = statement.executeQuery();
+            if (!game.next()) {
+                return null;
+            }
+            PlayerModel player = playerDaoJdbc.get(game.getString(3));
+            GameState gameState = new GameState(game.getString(2), game.getDate(1), player);
+            gameState.setId(id);
+            return gameState;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading game with id: " + id, e);
+        }
+
     }
 
     @Override
     public List<GameState> getAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, saved_at, current_map, player_name FROM game_state";
+            ResultSet result = conn.createStatement().executeQuery(sql);
+            List<GameState> games = new ArrayList<>();
+            while (result.next()) {
+                PlayerModel player = playerDaoJdbc.get(result.getString(4));
+                GameState gameState = new GameState(result.getString(3), result.getDate(2), player);
+                gameState.setId(result.getInt(1));
+                games.add(gameState);
+            }
+            return games;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all games", e);
+        }
     }
 }
--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDao.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface PlayerDao {
     void add(PlayerModel player);
     void update(PlayerModel player);
-    PlayerModel get(int id);
+    PlayerModel get(String playerName);
     List<PlayerModel> getAll();
 }
