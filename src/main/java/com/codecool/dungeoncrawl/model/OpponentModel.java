59024d5b7685e7cc5982858da6ce8abce7f94458
MichalRogalski22 <michalrogalsk@gmail.com>
2022-04-05 18:39:51 +0200

    Create OpponentModel class and its functions

---
+++ src/main/java/com/codecool/dungeoncrawl/model/OpponentModel.java
@@ -0,0 +1,80 @@
+package com.codecool.dungeoncrawl.model;
+
+import com.codecool.dungeoncrawl.logic.actors.Actor;
+import com.codecool.dungeoncrawl.logic.actors.Ghost;
+
+public class OpponentModel extends BaseModel{
+    private String opponentName;
+    private int x;
+    private int y;
+    private int hp;
+    private int power;
+    private int shield;
+
+    public OpponentModel(Actor opponent){
+        this.opponentName = opponent.getTileName();
+        this.x = opponent.getX();
+        this.y = opponent.getY();
+        this.hp = opponent.getHealth();
+        this.power = opponent.getAttackPower();
+        this.shield = opponent.getShield();
+    }
+
+    public OpponentModel(String opponentName, int x, int y, int hp, int power, int shield){
+        this.opponentName = opponentName;
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+        this.power = power;
+        this.shield = shield;
+    }
+    public String getOpponentName() {
+        return opponentName;
+    }
+
+    public void setOpponentName(String playerName) {
+        this.opponentName = playerName;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+
+    public void setShield(int shield) {
+        this.shield = shield;
+    }
+}
+
+
